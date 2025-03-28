d79de6299477569b4172bf1b9b5ad6e9404ed170
EricArcherman <ericarcherman@gmail.com>
2025-03-28 13:25:29 -0400

    cbor to json parser [not done]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use serde::{Deserialize, Serialize};
-use serde_cbor::{from_slice, to_vec};
+use serde_cbor::from_slice;
 use std::fs::{File, read};
 use std::io::Write;
+use walkdir::WalkDir;
+use std::collections::HashMap;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Walk through all directories in the data folder
+    for entry in WalkDir::new("omc-regs-fib-hyperkzg-benchmark-results/data")
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let path = entry.path();
+
+        println!("{}", path.display());
+    }
+
+    Ok(())
+}
 
